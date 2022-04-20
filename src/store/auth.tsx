3de9a5d840d3431fff7ed7c62e6ad196c4b3ccba
evelinaDakovska <evelina.dakovska@gmail.com>
2022-04-20 18:22:06 +0300

    feat: updated srore slices

--- src/store/auth.tsx
+++ src/store/auth.tsx
@@ -1,18 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialAuthState = { isAuth: true };
+const initialAuthState = {
+  isAuth: false,
+  email: "",
+  uid: "",
+  fName: "",
+  lName: "",
+  avatar: "",
+};
 
 const authSlice = createSlice({
   name: "auth",
   initialState: initialAuthState,
   reducers: {
-    login(state) {
+    login(
+      state,
+      {
+        payload: { email, uid, fName, lName, avatar },
+      }: PayloadAction<{
+        email: string;
+        uid: string;
+        fName: string;
+        lName: string;
+        avatar: "";
+      }>
+    ) {
       state.isAuth = true;
+      state.email = email;
+      state.uid = uid;
+      state.fName = fName;
+      state.lName = lName;
+      state.avatar = avatar;
     },
     logout(state) {
       state.isAuth = false;
+      state.email = "";
+      state.uid = "";
+      state.fName = "";
+      state.lName = "";
+      state.avatar = "";
     },
   },
 });
-export const authActions = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
