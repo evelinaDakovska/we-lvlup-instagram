1088c7796fc7ebc988cf4a2c9b2923b0f83762f1
evelinaDakovska <evelina.dakovska@gmail.com>
2022-05-03 17:20:42 +0300

    feat: state persistance

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,12 +3,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter";
 import authReducer from "./auth";
+import { loadState, saveState } from "./localStorage";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
   },
+  preloadedState: loadState(),
 });
+
+store.subscribe(() => {
+  saveState({ auth: store.getState().auth });
+});
+
 export const { dispatch } = store;
 export default store;
---
+++ src/store/localStorage.tsx
@@ -0,0 +1,20 @@
+export const loadState = () => {
+  try {
+    const serializeState: any = localStorage.getItem("state");
+    if (!serializeState) {
+      return undefined;
+    }
+    return JSON.parse(serializeState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state: any) => {
+  try {
+    const serializeState = JSON.stringify(state);
+    localStorage.setItem("state", serializeState);
+  } catch (err) {
+    console.log(err);
+  }
+};
