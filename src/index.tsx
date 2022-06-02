2bcb55a15446fa1b936fb3e421a5afa0d181d783
evelinaDakovska <evelina.dakovska@gmail.com>
2022-06-02 12:39:37 +0300

    fix: main root updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import store from "./store/index";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
